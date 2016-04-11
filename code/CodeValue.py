1e54955c36a32b4fada60461fa51fcae9a9232af
y.aoki <y.aoki@192.168.1.53>
2016-04-11 11:10:54 +0900

    修正

--- code/CodeValue.py
+++ code/CodeValue.py
@@ -1,12 +1,5 @@
 class CodeValue():
     """コード値"""
-    
     def __init__(self, c, n):
         self.__code = c;
         self.__name = n;
-
-    __code =0   #コード値
-    __name =""  #名称
-    
-    
-    
